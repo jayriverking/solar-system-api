ee45b77dfb8430d99ffeb7012b14883a81aac5a5
Stacy <stacydodson5@gmail.com>
2023-04-25 11:24:59 -0400

    Finished wave 2

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 # add moon class!
 
@@ -14,6 +14,19 @@ planets = [
     Planet(2, "Mars", "A dusty, cold, desert world with a very thin atmosphere", 2),
     Planet(3, "Pluto", "Pluto is very small, only about half the width of the United States", 5)
 ]
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message": f"planet {planet_id} is not a valid input type"}, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+    
+    abort(make_response({"message: " :f"planet id {planet_id} does not exist"}, 404))
+
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 @planets_bp.route("", methods=["GET"])
@@ -29,3 +42,15 @@ def handle_planets():
     return jsonify(planets_response)
 
 # wave 02
+
+@planets_bp.route("/<planet_id>", methods = ["GET"])
+
+def handle_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    return {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "moons": planet.moons
+    }
