12878028dee2163003ddf153d1e29f80c9450971
Jay King <jayriverking@gmail.com>
2023-05-02 11:06:30 -0400

    wave 04 done

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,19 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
+# validate helper function
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message": f"{planet_id} is not a valid type! {type(planet_id)} must be an integer"}, 400))
+    
+    planet = Planet.query.get(planet_id)
+    if not planet:
+        abort(make_response({"message": f"Planet {planet_id} does not exist."}, 404))
+
+    return planet
+
 
 @planets_bp.route("", methods=["POST"])
 def make_a_planet():
@@ -34,6 +47,46 @@ def get_all_planets():
             "description": planet.description
         })
     return jsonify(planet_response)
+
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description
+    }
+
+
+@planets_bp.route("/<planet_id>", methods=["PUT"])
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+
+    db.session.commit()
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description
+    }
+
+
+
+@planets_bp.route("/<planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+    db.session.delete(planet)
+    db.session.commit()
+    return make_response(f"Planet {planet_id}: {planet.name} successfully deleted")
+
+
+
 
 # class Moon:
 #     def __init__(self, id, name):
